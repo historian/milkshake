142edde995e65f278ab61379c1ddf4201a74ec6c
Simon Menke <simon.menke@gmail.com>
2011-01-27 15:28:36 +0100

    fix

--- lib/milkshake/environment.rb
+++ lib/milkshake/environment.rb
@@ -6,22 +6,31 @@ module Milkshake
     
     def initialize(cache)
       @cache = cache
-      reload!
     end
     
     def reload!
-      groups = [:default, Rails.env.to_s.to_sym]
-      @gemspecs = Bundler.definition.specs_for(groups)
+      @gemspecs = nil
     end
     
     def gemspecs
+      load_gemspecs
       @gemspecs
     end
     
     def gem_dependencies
+      load_gemspecs
       @gemspecs.map do |spec|
         Rails::GemDependency.new(spec.name, :lib => false, :version => spec.version)
       end
+    end
+    
+  private
+    
+    def load_gemspecs
+      return if @gemspecs
+      
+      groups = [:default, Rails.env.to_s.to_sym]
+      @gemspecs = Bundler.definition.specs_for(groups)
     end
     
   end
